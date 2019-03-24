c36e3a57883fb776cfbc5ec4a70d45f8644dbee6
Himank Jog <himank.jog@hashedin.com>
2019-03-24 15:51:08 +0530

    Updated issues model

--- src/models/issue.ts
+++ src/models/issue.ts
@@ -1,14 +1,38 @@
 export class Issue {
+    private id: number;
+    private created_at: Date;
+    private updated_at: Date;
+    private title: string;
+    private description: string;
+    private status: string;
+    private severity: number;
+    private due_date: Date;
+    private type: string;
+    private project: number;
+    private assignee: string;
   constructor(
-    private id: number,
-    private created_at: Date,
-    private updated_at: Date,
-    private title: string,
-    private description: string,
-    private status: string,
-    private severity: number,
-    private due_date: Date,
-    private type: string,
-    private project: number,
-    private assignee: string) {}
+    id,
+    created_at,
+    updated_at,
+    title,
+    description,
+    status,
+    severity,
+    due_date,
+    type,
+    project,
+    assignee
+  ) {
+  this.id = id;
+  this.created_at = created_at;
+  this.updated_at = updated_at;
+  this.title = title;
+  this.description = description;
+  this.status = status;
+  this.severity = severity;
+  this.due_date = due_date;
+  this.type = type;
+  this.project = project;
+  this.assignee = assignee;
+  }
 }
